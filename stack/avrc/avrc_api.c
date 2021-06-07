84ac5f95f35edcfcf3f83fc8885ba4c9248e10dd
syphyr <syphyr@gmail.com>
2021-06-07 19:03:40 +0200

    avrcp: Fix usage of p_drop_msg
    p_drop_msg depends on BT_USE_TRACES equals true, otherwise the
    variable is not initialized.

--- stack/avrc/avrc_api.c
+++ stack/avrc/avrc_api.c
@@ -648,7 +648,9 @@ static void avrc_msg_cback(UINT8 handle, UINT8 label, UINT8 cr,
                                            __func__, p_pkt->len, AVRC_OP_UNIT_INFO_RSP_LEN);
                         android_errorWriteLog(0x534e4554, "79883824");
                         drop = true;
+#if (BT_USE_TRACES == TRUE)
                         p_drop_msg = "UNIT_INFO_RSP too short";
+#endif
                         break;
                     }
                     p_data += 4; /* 3 bytes: ctype, subunit*, opcode + octet 3 (is 7)*/
@@ -688,7 +690,9 @@ static void avrc_msg_cback(UINT8 handle, UINT8 label, UINT8 cr,
                                            __func__, p_pkt->len, AVRC_OP_SUB_UNIT_INFO_RSP_LEN);
                         android_errorWriteLog(0x534e4554, "79883824");
                         drop = true;
+#if (BT_USE_TRACES == TRUE)
                         p_drop_msg = "SUB_UNIT_INFO_RSP too short";
+#endif
                         break;
                     }
                     p_data += AVRC_AVC_HDR_SIZE; /* 3 bytes: ctype, subunit*, opcode */
